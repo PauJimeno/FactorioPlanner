dba9abbbc4f49da3746e6a3492a0c657099a0014
PauJimeno <102144662+PauJimeno@users.noreply.github.com>
2024-08-14 09:35:38 +0200

    Update FactoryLogic documentation
    Documented FactoryLogic class

--- model/FactoryLogic.py
+++ model/FactoryLogic.py
@@ -2,6 +2,24 @@ from z3 import *
 
 
 class FactoryLogic:
+    """
+    This class conatins the logic that prevents any collisions between all factory elements.
+
+    :param width: Width of the blueprint
+    :type width: Int
+
+    :param height: Height of the blueprint
+    :type height: Int
+
+    :param conveyor: Reference to the conveyor directions variable
+    :type conveyor: Array[Array] EnumSort
+
+    :param inserter: Reference to the inserter directions variable
+    :type inserter: Array[Array] EnumSort
+    
+    :param assembler: Reference to the assembler collision_area variable
+    :type assembler: Array[Array] BitVec
+    """
     def __init__(self, width, height, conveyor, inserter, assembler):
         self.conveyor = conveyor.conveyor
         self.conveyor_dir = conveyor.direction
@@ -12,6 +30,12 @@ class FactoryLogic:
         self.height = height
 
     def collision(self):
+        """
+        Creates a constarint that ensures that all elements of the blueprint (conveyors, inserters and assemblers) don't collide between each other
+
+        :return: the constraint that ensures there can't be any elements in the same cell of the blueprint
+        :rtype: Array
+        """
         return [PbLe([(self.inserter[i][j] != self.inserter_dir[0], 1),
                       (self.conveyor[i][j] != self.conveyor_dir[0], 1)], 1)
                 for i in range(self.height) for j in range(self.width)] +\
@@ -21,6 +45,12 @@ class FactoryLogic:
                  for i in range(self.height) for j in range(self.width)]
 
     def constraints(self):
+        """
+        Creates a list of all the constarints representing the logic of the class
+
+        :return: the constraint that ensures there can't be any elements in the same cell of the blueprint
+        :rtype: Array
+        """
         return self.collision()
 
 
