8125234f7cbacaeb60871b978cb7ca84bcb45f64
PauJimeno <102144662+PauJimeno@users.noreply.github.com>
2024-08-14 09:48:11 +0200

    Update DirectionalElement documentation
    Added documentation to the class DirectionalElement

--- model/DirectionalElement.py
+++ model/DirectionalElement.py
@@ -2,10 +2,14 @@ from z3 import *
 
 
 class DirectionalElement:
-    # Abstract class that implements shared behaviours between directional elements in the model
+    """
+    Abstract class that implements shared behaviours between directional elements in the model
+    """
+    # The EnumSort type is a global variabel of the class because there can only be one declaration of the same type
     dir_type, direction = EnumSort('direction', ['empty', 'north', 'east', 'south', 'west'])
 
     def __init__(self):
+        # Position relative to (0,0) after moving in each direction
         self.displacement = {
             1: (-1, 0),  # North
             2: (0, 1),   # East
@@ -13,6 +17,7 @@ class DirectionalElement:
             4: (0, -1)   # West
         }
 
+        # Opposite position relative to (0,0) after moving in each direction
         self.opposite_dir = {
             1: self.direction[3],  # North -> South
             2: self.direction[4],  # East  -> West
@@ -20,6 +25,7 @@ class DirectionalElement:
             4: self.direction[2]   # West  -> East
         }
 
+        # Opposite direction for each direction
         self.opposite_num_dir = {
             1: 3,  # North -> South
             2: 4,  # East  -> West
